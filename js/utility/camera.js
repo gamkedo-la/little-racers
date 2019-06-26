2092374a7ed2a28bbbdc9fa027f0bac22fad4267
vmckeown <vmckeown79@gmail.com>
2019-06-25 21:37:22 -0400

    Camera Edge enforcement

--- js/utility/camera.js
+++ js/utility/camera.js
@@ -4,6 +4,22 @@ var camPanY = 0;
 function updatedCameraPosition(){
 	camPanX = vehicleList[0].x - (canvas.width/2)/scaleWidth;
 	camPanY = vehicleList[0].y - (canvas.height/2)/scaleHeight;
+	if(camPanX < 0){
+		camPanX = 0;
+	}
+	if(camPanY < 0){
+		camPanY = 0;
+	}
+	
+	var rightEdgeX = TRACK_W * TRACK_COLS;
+	var bottomEdgeY = TRACK_H * TRACK_ROWS;
+	
+	if(camPanX >= rightEdgeX - 1 - canvas.width/scaleWidth){
+		camPanX = rightEdgeX - 1 - canvas.width/scaleWidth ;
+	}
+	if(camPanY >= bottomEdgeY - 1 - canvas.height/scaleHeight){
+		camPanY = bottomEdgeY - 1 - canvas.height/scaleHeight;
+	}
 }
 
 function shiftForCameraPan(){
