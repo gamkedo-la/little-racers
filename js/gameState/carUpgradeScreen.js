dcf92e1e5a963e5cd1a66740e8f147fc4a4fed4e
vmckeown <vmckeown79@gmail.com>
2019-06-29 00:39:37 -0400

    On the Car Upgrade Screen.  I removed the hard numbers and replaced them all with vars.  This will make it easier in the feature to re-arranged this page as new items get added, or things need to move around.

--- js/gameState/carUpgradeScreen.js
+++ js/gameState/carUpgradeScreen.js
@@ -3,6 +3,12 @@ var tireOptions = false;
 var engineOptions = false;
 var nitroOptions = false;
 var exhaustOptions = false;
+var transmissionBoxX = 60;
+var transmissionBoxY = 360;
+var tireBoxX = 60;
+var tireBoxY = 460;
+var engineBoxX = 375;
+var engineBoxY = 290;
 var optionOneX = 500;
 var optionOneY = 10;
 var optionTwoX = 500;
@@ -11,8 +17,14 @@ var optionThreeX = 500;
 var optionThreeY = 300
 var optionFourX = 500;
 var optionFourY = 450;
-var purchaseBoxX = 610;
-var purchaseBoxY = 100;
+var purchaseBoxX = 20;
+var purchaseBoxY = 150;
+var nitroBoxX = 60;
+var nitroBoxY = 290;
+var exhaustBoxX = 375;
+var exhaustBoxY = 510;
+var exitBoxX = 300;
+var exitBoxY = 150;
 var option1 = false;
 var option2 = false;
 var option3 = false;
@@ -27,18 +39,19 @@ function drawCarUpgradeScreen(){
 	canvasContext.drawImage(bodyShopPic, 0, 0);
 	colorText("Body Shop", 120, 50, 'black', font = "24px Arial Black");
 	colorText("Cash: $" + vehicleList[0].cash, 50, 100, font = "12px Arial Black");
-	colorRect(60,360,100,50, 'blue'); //transmission	
-	colorRect(60,460,100,50, 'blue');	//tires
-	colorRect(375,290,100,50, 'blue');	//engine
-	colorRect(60,290,100,50, 'blue');	//nitro
-	colorRect(375,510,100,50, 'blue');	//exhaust 
-	colorRect(300,150,100,50,'red'); //exit shop
-	colorText("Transmission", 62, 390, 'white', font = "12px Arial Black");
-	colorText("Tires", 88, 490, 'white', font = "14px Arial Black");
-	colorText("Engine", 395, 320, 'white', font = "14px Arial Black");
-	colorText("Nitro", 88, 320, 'white', font = "14px Arial Black");
-	colorText("Exhaust", 390, 540, 'white', font = "14px Arial Black");
-	colorText("Exit Shop", 315, 180, 'black', font = "14px Arial Black");
+	colorRect(transmissionBoxX, transmissionBoxY,100,50, 'blue'); //transmission	
+	colorRect(tireBoxX, tireBoxY,100,50, 'blue');	//tires
+	colorRect(engineBoxX, engineBoxY,100,50, 'blue');	//engine
+	colorRect(nitroBoxX, nitroBoxY,100,50, 'blue');	//nitro
+	colorRect(exhaustBoxX, exhaustBoxY,100,50, 'blue');	//exhaust 
+	colorRect(exitBoxX, exitBoxY,100,50,'red'); //exit shop
+	colorText("Transmission", transmissionBoxX + 2, transmissionBoxY + 30, 'white', font = "12px Arial Black");
+	colorText("Tires", tireBoxX + 20, tireBoxY + 30, 'white', font = "14px Arial Black");
+	colorText("Engine", engineBoxX + 20, engineBoxY + 30, 'white', font = "14px Arial Black");
+	colorText("Nitro", nitroBoxX + 28, nitroBoxY + 30, 'white', font = "14px Arial Black");
+	colorText("Exhaust", exhaustBoxX + 15, exhaustBoxY + 30, 'white', font = "14px Arial Black");
+	colorText("Exit Shop", exitBoxX + 15, exitBoxY + 30, 'black', font = "14px Arial Black");
+	
 	if(optionsSelected){
 		if(option1){
 			colorRect(optionOneX, optionOneY,100,100, 'green'); 
@@ -61,7 +74,7 @@ function drawCarUpgradeScreen(){
 			colorRect(optionFourX,optionFourY,100,100, 'blue');
 		}
 		if(purchaseBox){
-			colorRect(purchaseBoxX,purchaseBoxY,100,50, 'green');
+			colorRect(purchaseBoxX,purchaseBoxY,100,50, 'white');
 			colorText("Purchase", purchaseBoxX + 20, purchaseBoxY + 20, 'black', font = "10px Arial Black");
 			colorText(purchaseItem, purchaseBoxX + 10, purchaseBoxY + 40, 'black', font = "10px Arial Black");
 		}
@@ -71,17 +84,17 @@ function drawCarUpgradeScreen(){
 		optionsSelected = true;
 		colorRect(250,290,30,200, 'red');
 		colorText("Transmission", 62, 390, 'red', font = "12px Arial Black");
-		colorText("Tx Option 1", 505, 50, 'white', font = "12px Arial Black"); // To be replaced with a pic
-		colorText("Tx Option 2", 505, 205, 'white', font = "12px Arial Black"); 
-		colorText("Tx Option 3", 505, 355, 'white', font = "12px Arial Black");
-		colorText("Tx Option 4", 505, 505, 'white', font = "12px Arial Black");
+		colorText("Tx Option 1", optionOneX + 5, 50, 'white', font = "12px Arial Black"); // To be replaced
+		colorText("Tx Option 2", optionTwoX + 5, 205, 'white', font = "12px Arial Black"); // to be replaced
+		colorText("Tx Option 3", optionThreeX + 5, 355, 'white', font = "12px Arial Black"); // to be replaced 
+		colorText("Tx Option 4", optionFourX + 5, 505, 'white', font = "12px Arial Black"); // to be replaced
 	} else if(tireOptions){
 		optionsSelected = true;
 		colorRect(168,283,18,50, 'red'); //left front tire
 		colorRect(338,283,18,50, 'red'); //right front tire
 		colorRect(168,457,18,58, 'red'); // left back tire
 		colorRect(338,457,18,58, 'red'); // right back tire
-		colorText("Tires", 88, 490, 'red', font = "14px Arial Black");
+		colorText("Tires", tireBoxX + 20, tireBoxY + 30, 'red', font = "14px Arial Black");
 		//Tire Option 1
 		canvasContext.drawImage(tireOptionOnePic, optionOneX, optionOneY);
 		colorText("Basic Tires", optionOneX + 18, optionOneY + 115, 'black', font = "10px Arial Black");
@@ -101,15 +114,15 @@ function drawCarUpgradeScreen(){
 	} else if(engineOptions){
 		optionsSelected = true;
 		colorRect(215,290,90,50, 'red');
-		colorText("Engine", 395, 320, 'red', font = "14px Arial Black");		
-		colorText("En Option 1", 505, 50, 'white', font = "12px Arial Black"); 
-		colorText("En Option 2", 505, 205, 'white', font = "12px Arial Black");
-		colorText("En Option 3", 505, 355, 'white', font = "12px Arial Black");
-		colorText("En Option 4", 505, 505, 'white', font = "12px Arial Black");
+		colorText("Engine", engineBoxX + 20, engineBoxY + 30, 'red', font = "14px Arial Black");		
+		colorText("En Option 1", optionOneX + 5, 50, 'white', font = "12px Arial Black");  //to be replaced
+		colorText("En Option 2", 505, 205, 'white', font = "12px Arial Black");  //to be replaced
+		colorText("En Option 3", 505, 355, 'white', font = "12px Arial Black");  //to be replaced
+		colorText("En Option 4", 505, 505, 'white', font = "12px Arial Black");  //to be replaced
 	} else if(nitroOptions){
 		optionsSelected = true;
 		colorRect(220,350,20,20, 'red'); 
-		colorText("Nitro", 88, 320, 'red', font = "14px Arial Black");
+		colorText("Nitro", nitroBoxX + 28, nitroBoxY + 30, 'red', font = "14px Arial Black");
 		//Tire Option 1
 		canvasContext.drawImage(nitrosOptionOnePic, optionOneX, optionOneY);
 		colorText("Basic Nitros", optionOneX + 18, optionOneY + 115, 'black', font = "10px Arial Black");
@@ -130,93 +143,140 @@ function drawCarUpgradeScreen(){
 		optionsSelected = true;
 		colorRect(225,520,20,10, 'red');
 		colorRect(280,520,20,10, 'red');
-		colorText("Exhaust", 390, 540, 'red', font = "14px Arial Black");
-		colorText("Ex Option 1", 505, 50, 'white', font = "12px Arial Black"); // To be replaced with a pic
-		colorText("Ex Option 2", 505, 205, 'white', font = "12px Arial Black");
-		colorText("Ex Option 3", 505, 355, 'white', font = "12px Arial Black");
-		colorText("Ex Option 5", 505, 505, 'white', font = "12px Arial Black");
+		colorText("Exhaust", exhaustBoxX + 15, exhaustBoxY + 30, 'red', font = "14px Arial Black");
+		colorText("Ex Option 1", optionOneX + 5, 50, 'white', font = "12px Arial Black");  //to be replaced 
+		colorText("Ex Option 2", optionTwoX + 5, 205, 'white', font = "12px Arial Black");  //to be replaced
+		colorText("Ex Option 3", optionThreeX + 5, 355, 'white', font = "12px Arial Black");  //to be replaced
+		colorText("Ex Option 5", optionFourX + 5, 505, 'white', font = "12px Arial Black");  //to be replaced
 	}
 }
 	
 function carUpgradeScreenMouseClick(mousePosX, mousePosY) {
-	console.log(mousePosX, mousePosY);
-	if(mousePosX > 60 * scaleWidth && mousePosX < 160 * scaleWidth && mousePosY > 360 * scaleHeight  && mousePosY < 410 * scaleHeight ){ //Transmission
+	console.log(mousePosX * scaleWidth, mousePosY * scaleHeight);
+	//Transmission
+	if(		mousePosX > transmissionBoxX * scaleWidth && mousePosX < (transmissionBoxX + 100) * scaleWidth && 
+			mousePosY > transmissionBoxY * scaleHeight && mousePosY < (transmissionBoxY + 50) * scaleHeight ){ 
 		turnOffOtherOptions();
 		turnOffOption1through4();
 		transmissionOptions = true;
-	} else if(mousePosX > 60 * scaleWidth && mousePosX < 160 * scaleWidth && mousePosY > 460 * scaleHeight  && mousePosY < 510 * scaleHeight ){  // Tires  
+	// Tires  
+	} else if(	mousePosX > tireBoxX * scaleWidth && mousePosX < (tireBoxX + 100) * scaleWidth && 
+				mousePosY > tireBoxY * scaleHeight && mousePosY < (tireBoxY + 50) * scaleHeight ){  
 		turnOffOtherOptions();
 		turnOffOption1through4();
 		tireOptions = true;
-	} else if(mousePosX > 375 * scaleWidth && mousePosX < 475 * scaleWidth && mousePosY > 290 * scaleHeight  && mousePosY < 340 * scaleHeight ){ //Engine
+	//Engine
+	} else if(	mousePosX > engineBoxX * scaleWidth && mousePosX < (engineBoxX + 100) * scaleWidth && 
+				mousePosY > engineBoxY * scaleHeight  && mousePosY < (engineBoxY + 50) * scaleHeight ){ 
 		turnOffOtherOptions();
 		turnOffOption1through4();
 		engineOptions = true;
-	} else if(mousePosX > 88 * scaleWidth && mousePosX < 188 * scaleWidth && mousePosY > 290 * scaleHeight  && mousePosY < 340 * scaleHeight ){ //Nitro
+	//Nitro
+	} else if(	mousePosX > nitroBoxX * scaleWidth && mousePosX < (nitroBoxX + 100) * scaleWidth && 
+				mousePosY > nitroBoxY * scaleHeight && mousePosY < (nitroBoxY + 50) * scaleHeight ){ 
 		turnOffOtherOptions();
 		turnOffOption1through4();
 		nitroOptions = true;
-	} else if(mousePosX > 375 * scaleWidth && mousePosX < 475 * scaleWidth && mousePosY > 510 * scaleHeight  && mousePosY < 560 * scaleHeight ){ //Exhaust
+	//Exhaust
+	} else if(	mousePosX > exhaustBoxX * scaleWidth && mousePosX < (exhaustBoxX + 100) * scaleWidth && 
+				mousePosY > exhaustBoxY * scaleHeight  && mousePosY < (exhaustBoxY + 50) * scaleHeight ){ 
 		turnOffOtherOptions();
 		turnOffOption1through4();
 		exhaustOptions = true;
-	} else if(mousePosX > 300 * scaleWidth && mousePosX < 400 * scaleWidth && mousePosY > 150 * scaleHeight  && mousePosY < 200 * scaleHeight ){ //Exit the shop
+	//Exit the shop
+	} else if(	mousePosX > exitBoxX * scaleWidth && mousePosX < (exitBoxX + 100) * scaleWidth && 
+				mousePosY > exitBoxY * scaleHeight  && mousePosY < (exitBoxY + 50) * scaleHeight ){ 
 		carUpgradeScreen = false;
-	} else if(mousePosX > 500 * scaleWidth && mousePosX < 600 * scaleWidth && mousePosY > 20 * scaleHeight && mousePosY < 120 * scaleHeight){ // Option Box 1
+	//Option Box 1
+	} else if(	mousePosX > optionOneX * scaleWidth && mousePosX < (optionOneX + 100) * scaleWidth && 
+				mousePosY > optionOneY * scaleHeight && mousePosY < (optionOneY + 100) * scaleHeight){ 
 		if(transmissionOptions){
 			turnOffOption1through4();
 			option1 = true;
+			purchaseBox = true;
+			purchaseItem = "Transmission 1";
 		} else if(tireOptions){
 			turnOffOption1through4();
 			option1 = true;
+			purchaseBox = true;
+			purchaseItem = "Tire 1";
 		} else if(engineOptions){
 			turnOffOption1through4();
 			option1 = true;
+			purchaseBox = true;
+			purchaseItem = "Engine 1";
 		} else if(nitroOptions){
 			turnOffOption1through4();
 			option1 = true;
+			purchaseBox = true;
+			purchaseItem = "Nitro 1";
 		} else if(exhaustOptions){
 			turnOffOption1through4();
-			option1 = true;
+			option1 = true
+			purchaseBox = true;
+			purchaseItem = "Exhaust 1";
 		}
-	} else if(mousePosX > 500 * scaleWidth && mousePosX < 600 * scaleWidth && mousePosY > 175 * scaleHeight && mousePosY < 275 * scaleHeight){ // Option Box 2
+	// Option Box 2
+	} else if(	mousePosX > optionTwoX * scaleWidth && mousePosX < (optionTwoX + 100) * scaleWidth && 
+				mousePosY > optionTwoY * scaleHeight && mousePosY < (optionTwoY + 100) * scaleHeight){ 
 		if(transmissionOptions){
 			turnOffOption1through4();
 			option2 = true;
+			purchaseBox = true;
+			purchaseItem = "Transmission 2";
 		} else if(tireOptions){
 			turnOffOption1through4();
-			option2 = true;
+			option2 = true
+			purchaseBox = true;
+			purchaseItem = "Tire 2";
 		} else if(engineOptions){
 			turnOffOption1through4();
-			option2 = true;
+			option2 = true
+			purchaseBox = true;
+			purchaseItem = "Engine 2";
 		} else if(nitroOptions){
 			turnOffOption1through4();
 			option2 = true;
+			purchaseBox = true;
+			purchaseItem = "Nitro 2";
 		} else if(exhaustOptions){
 			turnOffOption1through4();
 			option2 = true;
+			purchaseBox = true;
+			purchaseItem = "Exhaust 2";
 		}
-	} else if(mousePosX > 500 * scaleWidth && mousePosX < 600 * scaleWidth && mousePosY > 325 * scaleHeight && mousePosY < 425 * scaleHeight){ // Option Box 3
+	// Option Box 3
+	} else if(	mousePosX > optionThreeX * scaleWidth && mousePosX < (optionThreeX + 100) * scaleWidth && 
+				mousePosY > optionThreeY * scaleHeight && mousePosY < (optionThreeY + 100) * scaleHeight){ 
 		if(transmissionOptions){
 			turnOffOption1through4();
 			option3 = true;
-		} else if(transmissionOptions){
-			turnOffOption1through4();
-			option3 = true;
+			purchaseBox = true;
+			purchaseItem = "Transmission 3";
 		} else if(tireOptions){
 			turnOffOption1through4();
 			option3 = true;
+			purchaseBox = true;
+			purchaseItem = "Tire 3";
 		} else if(engineOptions){
 			turnOffOption1through4();
 			option3 = true;
+			purchaseBox = true;
+			purchaseItem = "Engine 3";
 		} else if(nitroOptions){
 			turnOffOption1through4();
-			option3 = true;
+			option3 = true			
+			purchaseBox = true;
+			purchaseItem = "Nitro 3";
 		} else if(exhaustOptions){
 			turnOffOption1through4();
 			option3 = true;
+			purchaseBox = true;
+			purchaseItem = "Exhaust 3";
 		}
-	} else if(mousePosX > 500 * scaleWidth && mousePosX < 600 * scaleWidth && mousePosY > 475 * scaleHeight && mousePosY < 575 * scaleHeight){ // Option Box 4
+	// Option Box 4
+	} else if(	mousePosX > optionFourX * scaleWidth && mousePosX < (optionFourX + 100) * scaleWidth && 
+				mousePosY > optionFourY * scaleHeight && mousePosY < (optionFourY + 100) * scaleHeight){
 		if(transmissionOptions){
 			turnOffOption1through4();
 			option4 = true;
@@ -231,7 +291,7 @@ function carUpgradeScreenMouseClick(mousePosX, mousePosY) {
 			turnOffOption1through4();
 			option4 = true;
 			purchaseBox = true;
-			purchaseItem = "Enginer 4";
+			purchaseItem = "Engine 4";
 		} else if(nitroOptions){
 			turnOffOption1through4();
 			option4 = true;
@@ -251,6 +311,7 @@ function turnOffOption1through4(){
 	option2 = false;
 	option3 = false;
 	option4 = false;
+	purchaseBox = false;
 }
 
 function turnOffOtherOptions(){
@@ -259,5 +320,6 @@ function turnOffOtherOptions(){
 	engineOptions = false;
 	nitroOptions = false;
 	exhaustOptions = false;
+	purchaseBox = false;
 }
 
