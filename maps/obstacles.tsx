fd5dac4d3b9579e0296c8b0db5b958143ea964f3
HTrayford <bowman_trayford@hotmail.com>
2019-07-11 06:29:14 +0300

    Updated locations of spritesheets in Tiled (since they were moved to a new project folder.

--- maps/obstacles.tsx
+++ maps/obstacles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="obstacles" tilewidth="40" tileheight="40" tilecount="50" columns="10">
- <image source="../images/trackobstaclesSpriteSheet.png" width="400" height="200"/>
+ <image source="../images/spriteSheet/trackobstaclesSpriteSheet.png" width="400" height="200"/>
 </tileset>
