2a506dd7aebbde7f062e9a784bc7502a5f84994a
Chris DeLeon <gamkedo@gmail.com>
2019-08-05 21:13:23 -0700

    Minimap line closer to road thickness, little car sprites added

--- js/minimap.js
+++ js/minimap.js
@@ -46,38 +46,64 @@ function miniMap() {
 
     this.draw = function() {
         canvasContext.save();
+        canvasContext.globalAlpha = 0.7;
         canvasContext.translate(miniMapX, miniMapY);
         canvasContext.scale(miniMapScale, miniMapScale);
-        for (var i = 0; i < levelList[levelNow].wayPointsX.length; i++){
+        canvasContext.beginPath();
+        canvasContext.strokeStyle = '#999999';
+        canvasContext.lineWidth = 115;
+        // the +1 overdraw covers a gap at the track start/end loop connection
+        var wayPointCount = levelList[levelNow].wayPointsX.length;
+        for (var i = 0; i < wayPointCount+1; i++){
             var prevI = i-1;
             if(prevI < 0) {
-                prevI = levelList[levelNow].wayPointsX.length-1;
+                prevI = wayPointCount-1;
             }
             var nextI = i+1;
-            if (nextI >= levelList[levelNow].wayPointsX.length){
-                nextI -= levelList[levelNow].wayPointsX.length;
-            }
+            nextI %= wayPointCount;
             var afterI = nextI+1;
-            if (afterI >= levelList[levelNow].wayPointsX.length){
-                afterI -= levelList[levelNow].wayPointsX.length;
-            }
+            afterI %= wayPointCount;
+            var loopedI = i; // needed to support the +1 overdraw segment
+            loopedI %= wayPointCount;
+
             /*
             colorLine(levelList[levelNow].wayPointsX[i], levelList[levelNow].wayPointsY[i],
                     levelList[levelNow].wayPointsX[nextI] , levelList[levelNow].wayPointsY[nextI], 'orange', canvasContext, 10);
             */
-            var prevX = levelList[levelNow].wayPointsX[i];
-            var prevY = levelList[levelNow].wayPointsY[i];
-            for(var ii=0;ii<=10;ii++) {
+            var prevX = levelList[levelNow].wayPointsX[loopedI];
+            var prevY = levelList[levelNow].wayPointsY[loopedI];
+            var segmentSplit = 10;
+
+            if(i==0) {
+                canvasContext.moveTo(prevX, prevY);
+            } else {
+                canvasContext.lineTo(prevX, prevY);
+            }
+            for(var ii=0;ii<=segmentSplit;ii++) {
                 this.interpPt(levelList[levelNow].wayPointsX[prevI], levelList[levelNow].wayPointsY[prevI],
-                    levelList[levelNow].wayPointsX[i], levelList[levelNow].wayPointsY[i],
+                    levelList[levelNow].wayPointsX[loopedI], levelList[levelNow].wayPointsY[loopedI],
                     levelList[levelNow].wayPointsX[nextI] , levelList[levelNow].wayPointsY[nextI],
                     levelList[levelNow].wayPointsX[afterI], levelList[levelNow].wayPointsY[afterI],
-                    ii/10.0);
-                colorLine(prevX, prevY, interpX, interpY, 'white', canvasContext, 15);
+                    ii/segmentSplit);
+                canvasContext.lineTo(interpX, interpY);
+
+                //colorLine(prevX, prevY, interpX, interpY, '#cccccc', canvasContext, 45);
                 prevX = interpX;
                 prevY = interpY;
             }
         }
+        canvasContext.stroke();
+
+        // draw little cars
+
+        canvasContext.translate(cameraP1.panX * miniMapScale, 
+                                cameraP1.panY * miniMapScale);
+
+        for (var i = 0; i < vehicleList.length; i++) {
+            vehicleList[i].drawCar(canvasContext);
+        }
+
+        // undo all the scaling and offsets        
         canvasContext.restore();
     }
 
