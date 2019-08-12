c5cbc43f63279d5cbe1e6dbe1bb2185cf181b870
vmckeown <vmckeown79@gmail.com>
2019-08-11 21:22:32 -0400

    Added @axphin 's Win Screen.  Still need to add vehicle finish times, cash payout, and pictures.

--- js/car/winner.js
+++ js/car/winner.js
@@ -6,6 +6,14 @@ var fifthPlace;
 var sixthPlace;
 var seventhPlace;
 var eigthPlace;
+var firstPlaceCarPic = vehicleList[0].myBitmap;
+var secondPlaceCarPic;
+var thirdPlaceCarPic;
+var fourthPlaceCarPic;
+var fifthPlaceCarPic;
+var sixthPlaceCarPic;
+var seventhPlaceCarPic;
+var eigthPlaceCarPic;
 var firstPlaceFilled = false;
 var secondPlaceFilled = false;
 var thirdPlaceFilled = false;
@@ -21,49 +29,57 @@ var soundDelayTimer = 0;
 function whichPlace(car){	
 	if(!firstPlaceFilled && !car.placedPosition){
 		firstPlace = car.myName;
+		firstPlaceCarPic = vehicleList[0].myBitmap;
 		firstPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 1000;
 		andTheWinnerIsSound.play();
 		console.log("First: " + car.myName + " " + car.cash);
 	} else if(!secondPlaceFilled && !car.placedPosition){
-		secondPlace = car.myName;
+		secondPlace = car.myName
+		secondPlaceCarPic = car.myCarPic;
 		secondPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 750;
 		console.log("Second: " + car.myName + " " + car.cash);	
 	} else if(!thirdPlaceFilled && !car.placedPosition){
 		thirdPlace = car.myName;
+		thirdPlaceCarPic = car.myCarPic;
 		thirdPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 500;
 		console.log("Third: " + car.myName + " " + car.cash);
 	} else if(!fourthPlaceFilled && !car.placedPosition){
 		fourthPlace = car.myName;
+		fourthPlaceCarPic = car.myCarPic;
 		fourthPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 250;
 		console.log("Fourth: " + car.myName + " " + car.cash);
 	} else if(!fifthPlaceFilled && !car.placedPosition){
 		fifthPlace = car.myName;
+		fifthPlaceCarPic = car.myCarPic;
 		fifthPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 100;
 		console.log("Fifth: " + car.myName + " " + car.cash);
 	} else if(!sixthPlaceFilled && !car.placedPosition){
 		sixthPlace = car.myName;
+		sixthPlaceCarPic = car.myCarPic;
 		sixthPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 50;
 		console.log("Sixth: " + car.myName + " " + car.cash);
 	} else if(!seventhPlaceFilled && !car.placedPosition){
 		seventhPlace = car.myName;
+		seventhPlaceCarPic = car.myCarPic;
 		seventhPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 25;
 		console.log("Seventh: " + car.myName + " " + car.cash);
 	} else if(!eigthPlaceFilled && !car.placedPosition){
 		eigthPlace = car.myName;
+		eigthPlaceCarPic = car.myCarPic;
 		eigthPlaceFilled = true;
 		car.placedPosition = true;
 		car.cash += 10;
