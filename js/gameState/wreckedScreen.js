3d4c58a3fdbed86ba88d53e4f5fdb16953f4c108
Simon J <sj.hoffiz@gmail.com>
2019-08-28 20:52:59 -0400

    Connected each car to its appropiate wreckedScreen color variation when player hits 0hp.

--- js/gameState/wreckedScreen.js
+++ js/gameState/wreckedScreen.js
@@ -3,8 +3,34 @@ function wreckedScreenMouseClick(mousePosX, mousePosY) {
 }
 
 function drawWreckedScreen(){
-
+	var wreckedScreenCarColor;
     canvasContext.clearRect(0,0,canvas.width,canvas.height);
+
+    if (vehicleList[0].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenGreenPic;
+    }
+    else if (vehicleList[1].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenYellowPic;
+    }
+    else if (vehicleList[2].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenOrangePic;
+    }
+    else if (vehicleList[3].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenPurplePic;
+    }
+    else if (vehicleList[4].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenRedPic;
+    }
+    else if (vehicleList[5].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenBluePic;
+    }
+    else if (vehicleList[6].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenLightgrayPic;
+    }
+    else if (vehicleList[7].healthRemaining <= 0) {
+        wreckedScreenCarColor = wreckedScreenDarkgrayPic;
+    }
  
-    canvasContext.drawImage(wreckedScreenYellowPic, 0, 0, canvas.width / scaleWidth, canvas.height / scaleHeight); // can't use canvas.width /2 due to scaling wierdness
+    canvasContext.drawImage(wreckedScreenCarColor, 0, 0, canvas.width / scaleWidth, canvas.height / scaleHeight); // can't use canvas.width /2 due to scaling wierdness
+
 }
